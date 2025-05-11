2fcff2f95f673854448a8ed523a42c4ebad14ff9
YourName <you@example.com>
2025-05-11 00:19:15 -0600

    My first commit

---
+++ demo-mh16015/src/main/java/com/iis/mh16015/demo_mh16015/App.java
@@ -0,0 +1,17 @@
+package com.iis.mh16015.demo_mh16015;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App 
+{
+    public String Hello() {
+        return "Hello World!";
+    }
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ demo-mh16015/src/test/java/com/iis/mh16015/demo_mh16015/AppTest.java
@@ -0,0 +1,19 @@
+package com.iis.mh16015.demo_mh16015;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello() {
+        assertTrue(true);
+    }
+}
